4acd245ea41ab9417d7bc4d41361478912ce689d
lew22 <richardgarmori@gmail.com>
2023-04-19 18:58:50 -0500

    primera aproximacion a recursividad

--- myapp/wscrapping.py
+++ myapp/wscrapping.py
@@ -5,21 +5,21 @@ from time import sleep
 from selenium.webdriver.common.by import By
 import os
 from selenium.webdriver.chrome.options import Options
-
+import requests
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def webscrappingMain(user,password):
-    chrome_options = webdriver.ChromeOptions()
-    prefs = {'download.default_directory' : 'C:\\Users\pc\Desktop\Descargas'}
-    chrome_options.add_experimental_option('prefs',prefs)
-    chromedriver = "C:\\Users\pc\Documents\djangoproject\chromedriver.exe"
-    chrome_options.add_experimental_option("detach", True)
+chrome_options = webdriver.ChromeOptions()
+prefs = {'download.default_directory' : 'C:\\Users\pc\Desktop\Descargas'}
+chrome_options.add_experimental_option('prefs',prefs)
+chromedriver = "C:\\Users\pc\Documents\djangoproject\chromedriver.exe"
+chrome_options.add_experimental_option("detach", True)
 
-    driver = webdriver.Chrome(executable_path=chromedriver,chrome_options=chrome_options)
-    
+driver = webdriver.Chrome(executable_path=chromedriver,chrome_options=chrome_options)
+
+def webscrappingMain(user,password):
     #abrimos la página web
     driver.get("https://aulavirtual.upc.edu.pe")
     
@@ -94,40 +94,72 @@ def webscrappingCourCont(courses_id,driver):
         lcourse = lbase+i+lend
         print("El link del curso: ", lcourse)
         linkCourses.append(lcourse)
+    
     #abrimos una pagina de cada curso y realizamos la lectura
-    driver.get(linkCourses[1])
-    sleep(3)
+    # driver.get(linkCourses[1])
+    driver.maximize_window()
+    curso = "https://aulavirtual.upc.edu.pe/webapps/blackboard/content/listContent.jsp?course_id=_367607_1&content_id=_39746882_1&mode=reset"
+    path =  "C:\\Users\pc\Desktop\Descargas"
+    depth = 3
+    scrape_page(curso,path,depth)
+    
+    # sleep(3)
     #Guardamos la ventana de los items de un curso
-    original_window = driver.current_window_handle
+    # original_window = driver.current_window_handle
 
     #verificamos que solo tengamos una ventana abierta
-    assert len(driver.window_handles) == 1
+    # assert len(driver.window_handles) == 1
 
-    wait = WebDriverWait(driver,10)
-    wait.until(EC.frame_to_be_available_and_switch_to_it("classic-learn-iframe"))
+    # wait = WebDriverWait(driver,10)
+    # wait.until(EC.frame_to_be_available_and_switch_to_it("classic-learn-iframe"))
 
-    sleep(5)
+    # sleep(5)
 
-    #verificaremos si el puller esta expandido o contraido
-    menupullercollapse = driver.find_element(By.XPATH, "//a[@aria-expanded='false']")
-    menupullerexpanded = driver.find_element(By.XPATH, "//a[@aria-expanded='true']")
+    # #verificaremos si el puller esta expandido o contraido
+    # menupullercollapse = driver.find_element(By.XPATH, "//a[@aria-expanded='false']")
+    # menupullerexpanded = driver.find_element(By.XPATH, "//a[@aria-expanded='true']")
 
-    if (menupullercollapse is not None):
-        menupullercollapse.click()
-    if (menupullerexpanded is not None):
-        print("puller expandido")
+    # if (menupullercollapse is not None):
+    #     menupullercollapse.click()
+    # if (menupullerexpanded is not None):
+    #     print("puller expandido")
 
     #ahora buscamos un ul que tenga li's y los guardamos
-    menucontent = driver.find_elements(By.XPATH, "//ul[@id='courseMenuPalette_contents']//li[@class='clearfix ']//a")
+    # menucontent = driver.find_elements(By.XPATH, "//ul[@id='courseMenuPalette_contents']//li[@class='clearfix ']//a")
     
-    data = dict()
+    # for i in menucontent:
+    #     span = i.find_element(By.TAG_NAME,'span')
+    #     span.get_attribute('title')
+    #     i.get_attribute('href')
+    #     # i.click()
+    # print(data)
 
-    for i in menucontent:
-        span = i.find_element(By.TAG_NAME,'span')
-        span.get_attribute('title')
-        i.get_attribute('href')
-        # i.click()
-    print(data)
 
-# def webscrappingFiles():
-#     print (os.getcwd())
+def scrape_page(url, folder_path,depth):
+    
+    if depth == 0:
+        return
+    
+    driver.get(url)
+    # Crear la carpeta para guardar los archivos descargados (si no existe)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    for link in links:
+        href = link.get_attribute('href')
+        onclick = link.get_attribute('onclick')
+
+        links = driver.find_elements(By.XPATH, "//h3//a")
+
+        # Si onclick no es nulo, es un archivo que se puede descargar
+        if onclick is not None:
+            response = requests.get(href)
+            filename = os.path.join(folder_path, os.path.basename(href))
+            with open(filename, 'wb') as f:
+                f.write(response.content)
+
+        # Si el enlace es una página, llamar a la función de scraping nuevamente
+        elif href.startswith('/webapps/'):
+            next_folder_path = os.path.join(folder_path, os.path.basename(href))
+            scrape_page(href, next_folder_path,depth-1)
+
